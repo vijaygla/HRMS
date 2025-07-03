78e5f4335a5f1a34641e63ee93c9eb341463e047
vijaykumarmganj1 <vijaykumarmganj1@gmail.com>
2025-07-03 06:45:27 +0530

    m

--- backend/middleware/notFound.js
+++
@@ -1,7 +0,0 @@
-export const notFound = (req, res, next) => {
-  const error = new Error(`Route not found - ${req.originalUrl}`);
-  res.status(404).json({
-    success: false,
-    message: error.message
-  });
-};
